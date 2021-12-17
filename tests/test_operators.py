127ceb4c87a8dbd028bdb17b428d782235a2d313
Anurag Singh <anuragsingh@Anurags-MacBook-Air.local>
2021-12-18 04:47:10 +0530

    Task 0.2 complete

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -104,49 +104,49 @@ def test_sigmoid(a):
     * It crosses 0 at 0.5
     * it is  strictly increasing.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    assert sigmoid(a) <= 1.0
+    assert sigmoid(a) >= 0.0
+    assert_close(1.0 - sigmoid(a), sigmoid(-a))
+    assert sigmoid(0) == 0.5
+    assert sigmoid(a) * (1.0 - sigmoid(a)) >= 0.0
 
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
 def test_transitive(a, b, c):
     "Test the transitive property of less-than (a < b and b < c implies a < c)"
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    if lt(a,b) and lt(b,c):
+        assert(lt(a,c))
 
 
 @pytest.mark.task0_2
-def test_symmetric():
+@given(small_floats,small_floats)
+def test_symmetric(a,b):
     """
     Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    None
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    assert mul(a,b) == mul(b,a)
 
 
 @pytest.mark.task0_2
-def test_distribute():
+@given(small_floats, small_floats, small_floats)
+def test_distribute(x,y,z):
     r"""
     Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    None
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    assert_close(mul(z,add(x, y)), add(mul(z, x), mul(z, y)))
 
 
 @pytest.mark.task0_2
-def test_other():
+@given(small_floats, small_floats, small_floats)
+def test_other(a,b,c):
     """
     Write a test that ensures some other property holds for your functions.
     """
-    None
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
-
+    assert_close(mul(a, mul(b, c)), mul(mul(a, b), c))
+    assert_close(add(a, add(b, c)), add(add(a, b), c))
 
 # ## Task 0.3  - Higher-order functions
 
